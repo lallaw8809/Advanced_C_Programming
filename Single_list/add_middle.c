a7b29e49eaf91b08c9fe33869335b821897186b4
lallaw8809 <lalbosco55@gmail.com>
2018-03-16 10:10:21 +0530

    Modified the middle node

--- Single_list/add_middle.c
+++ Single_list/add_middle.c
@@ -8,8 +8,6 @@
 *********************************************************************/
 struct node *add_middle(struct node *head)
 {
-	struct node *single_move = head;
-	struct node *double_move = head;
 	int data;
 
 	/* Create a new node */
@@ -22,21 +20,36 @@ struct node *add_middle(struct node *head)
 	
 	printf("Enter the data\n");
 	scanf("%d",&new_node->data);
-	
+
+	/* Validation of empty node */
 	if(head == NULL){
 		new_node->next = NULL;
-		return new_node;
+		head = new_node;
 	}
 	else{
-		while(double_move->next != NULL){
-			single_move = single_move->next;
-			double_move = double_move->next->next;
+		struct node *single_move = head;
+		struct node *double_move = head;
+		struct node *next_node   = NULL;
+
+		/* Find the middle node by move the single by once and
+		   Double_move by by twice.
+		   insert the node between.
+		 */
+		while(double_move != NULL){
+			double_move = double_move->next;
+			if(double_move != NULL)
+				double_move = double_move->next;
+
+			if(double_move == NULL){
+				next_node         = single_move->next;
+				single_move->next = new_node;
+				new_node->next    = next_node;
+			}
+			else
+				single_move = single_move->next;
 		}
-		single_move->next = new_node;
-		new_node->next    = double_move;
 	}
+
+	return head;
 }
-
-
-
 
